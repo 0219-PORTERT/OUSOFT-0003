2ee8c93a4e48bdd30e5220716640c821dcb69302
jikagq <12010824+jikagq@users.noreply.github.com>
2020-04-23 17:44:14 +0200

    Ajout du client hardware pour adc en cours
    Lien avec SCPI fonctionnel (*IDN ?) répond correctement
    Reste à modifier les fonctions decode et execute de can.cpp qui ne sont pas correcte

--- Integrationcppstatemachine/Src/Can.cpp
+++ Integrationcppstatemachine/Src/Can.cpp
@@ -6,13 +6,99 @@
  */
 
 #include "Can.h"
-
+#include "ScpiClientServer.h"
+#include <string>
+#include <iostream>
+#include "tm_stm32_adc.h"
 Can::Can() {
 	// TODO Auto-generated constructor stub
-
+	this->channel = TM_ADC_Channel_0;
 }
 
+Can::Can(std::string _name, TM_ADC_Channel_t _channel): ScpiClientServer(_name), channel(_channel) {
+
+}
 Can::~Can() {
 	// TODO Auto-generated destructor stub
 }
 
+short int Can::ExecuteCmde(std::string& _cmde, std::string& _rep) {
+	//_rep.assign("Je suis le execute de la classe Can");
+
+	switch (decodeInstruct(_cmde)) {
+
+	case REQ_RST:
+
+		break;
+	case REQ_IDN:
+		_rep.assign("je suis le client HARDWARE " + this->getHeader() +" et je suis sur la voie " + std::to_string(this->channel));
+		break;
+	case REQ_CH1:
+		_rep.assign(_cmde +" : "+std::to_string(TM_ADC_Read(ADC3, TM_ADC_Channel_0))+"\n\r");
+		break;
+	case REQ_CH2:
+		std::to_string(TM_ADC_Read(ADC3, TM_ADC_Channel_3));
+		break;
+	case REQ_CH3:
+		std::to_string(TM_ADC_Read(ADC3, TM_ADC_Channel_4));
+		break;
+	case REQ_CH4:
+		std::to_string(TM_ADC_Read(ADC3, TM_ADC_Channel_5));
+		break;
+	case REQ_CH5:
+		std::to_string(TM_ADC_Read(ADC3, TM_ADC_Channel_6));
+		break;
+	case REQ_CH6:
+		std::to_string(TM_ADC_Read(ADC3, TM_ADC_Channel_8));
+		break;
+	case REQ_CH7:
+		std::to_string(TM_ADC_Read(ADC3, TM_ADC_Channel_9));
+		break;
+	case REQ_CH8:
+		std::to_string(TM_ADC_Read(ADC3, TM_ADC_Channel_10));
+		break;
+	case REQ_CH9:
+		std::to_string(TM_ADC_Read(ADC3, TM_ADC_Channel_12));
+		break;
+	case REQ_CH10:
+		std::to_string(TM_ADC_Read(ADC3, TM_ADC_Channel_13));
+		break;
+	default:
+		//throw something;
+		break;
+	}
+
+	return 0;
+}
+
+int Can::decodeInstruct(std::string& _cmde) {
+
+	int sel = 0;
+
+	if (_cmde.compare("*RST") == 0) {
+		sel = REQ_RST;
+	} else if (_cmde.compare("*IDN ?") == 0) {
+		sel = REQ_IDN;
+	} else if(_cmde.compare("I_moy ?") == 0){
+
+	}
+	else if(_cmde.compare("I_moy ?") == 0){
+
+		}
+
+
+
+	else {
+		//throw something
+	}
+
+	return sel;
+}
+
+uint16_t readADC(TM_ADC_Channel_t channel){
+
+}
+
+uint16_t readADCmoy(TM_ADC_Channel_t _channel, int nb){
+
+}
--- Integrationcppstatemachine/Src/Can.h
+++ Integrationcppstatemachine/Src/Can.h
@@ -11,16 +11,36 @@
 #include "ScpiClientServer.h"
 #include <String>
 #include <iostream>
+#include "tm_stm32_adc.h"
 
 #define REQ_RST 1
 #define REQ_IDN 2
 #define REQ_x 3
-#define REQ_QST 4
+#define REQ_CH1 3
+#define REQ_CH2 4
+#define REQ_CH3 5
+#define REQ_CH4 6
+#define REQ_CH5 7
+#define REQ_CH6 8
+#define REQ_CH7 9
+#define REQ_CH8 10
+#define REQ_CH9 11
+#define REQ_CH10 12
+
+#define REQ_CH11
+#define REQ_CH12
 
 class Can : public ScpiClientServer {
 public:
 	Can();
+	Can(std::string _name, TM_ADC_Channel_t _channel);
 	virtual ~Can();
+
+private:
+	std::string _name;
+	TM_ADC_Channel_t channel;
+	short int ExecuteCmde (std::string& _cmde,std::string& _rep);
+	int decodeInstruct(std::string& _cmde);
 };
 
 #endif /* CAN_H_ */
