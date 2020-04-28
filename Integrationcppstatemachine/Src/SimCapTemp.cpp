e5011d90c0f3d7cedcfbf943372df003b6e9633a
jikagq <12010824+jikagq@users.noreply.github.com>
2020-04-28 15:25:02 +0200

    Ajout des fichiers pour potentiomètres numériques

---
+++ Integrationcppstatemachine/Src/SimCapTemp.cpp
@@ -0,0 +1,18 @@
+/*
+ * SimCapTemp.cpp
+ *
+ *  Created on: 28 avr. 2020
+ *      Author: PARIS
+ */
+
+#include "SimCapTemp.h"
+
+SimCapTemp::SimCapTemp() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SimCapTemp::~SimCapTemp() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Integrationcppstatemachine/Src/SimCapTemp.h
@@ -0,0 +1,17 @@
+/*
+ * SimCapTemp.h
+ *
+ *  Created on: 28 avr. 2020
+ *      Author: PARIS
+ */
+
+#ifndef SIMCAPTEMP_H_
+#define SIMCAPTEMP_H_
+
+class SimCapTemp {
+public:
+	SimCapTemp();
+	virtual ~SimCapTemp();
+};
+
+#endif /* SIMCAPTEMP_H_ */
