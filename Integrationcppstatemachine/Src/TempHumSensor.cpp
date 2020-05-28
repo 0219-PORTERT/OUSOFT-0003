12981a7c8140bfc9aba18c3daf5918cafe231e8d
jikagq <12010824+jikagq@users.noreply.github.com>
2020-05-28 09:26:23 +0200

    Ajout des nouveaux fichiers TempHumSensor cpp et h

---
+++ Integrationcppstatemachine/Src/TempHumSensor.cpp
@@ -0,0 +1,18 @@
+/*
+ * TempHumSensor.cpp
+ *
+ *  Created on: 28 mai 2020
+ *      Author: PARIS
+ */
+
+#include "TempHumSensor.h"
+
+TempHumSensor::TempHumSensor() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TempHumSensor::~TempHumSensor() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Integrationcppstatemachine/Src/TempHumSensor.h
@@ -0,0 +1,17 @@
+/*
+ * TempHumSensor.h
+ *
+ *  Created on: 28 mai 2020
+ *      Author: PARIS
+ */
+
+#ifndef TEMPHUMSENSOR_H_
+#define TEMPHUMSENSOR_H_
+
+class TempHumSensor {
+public:
+	TempHumSensor();
+	virtual ~TempHumSensor();
+};
+
+#endif /* TEMPHUMSENSOR_H_ */
